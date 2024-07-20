104a70f6feb635072e6838f09122a18ef0d94868
jandromagno91 <astanl60@students.kennesaw.edu>
2024-07-19 21:42:01 -0400

    finished images and sounds. complete project

--- average_mode.py
+++ average_mode.py
@@ -5,9 +5,10 @@ import sys
 
 pygame.init()
 
-width, height = 1000, 1000
+width, height = 1000, 750
 screen = pygame.display.set_mode((width, height))
 
+#colors
 white = (255, 255, 255)
 red = (255, 0, 0)
 blue = (0, 0, 255)
@@ -15,8 +16,17 @@ green = (0, 255, 0)
 gray = (200, 200, 200)
 black = (0, 0, 0)
 
-font = pygame.font.Font(None, 36)
+#timer and fonts
+timer = 30
+countdown = 3
+last_count = pygame.time.get_ticks()
+clock = pygame.time.Clock()
+fps = 60
+font30 = pygame.font.SysFont('Constantia', 30)
+font40 = pygame.font.SysFont('Constantia', 40)
+font50 = pygame.font.SysFont('Constantia', 50)
 
+#Dimentions
 player_size = 40
 obstacle_width = 110
 obstacle_height = 60
@@ -24,43 +34,107 @@ obstacle_speed = 8
 safe_area_height = 100
 middle_height = height - 2 * safe_area_height
 lanes = 3
-lane_height = obstacle_height + 25
-cars = 95 #(0-100)lower number to increase the chance of adding an obstacle
+lane_height = 65
+cars = 92 #(0-100)lower number to increase the chance of adding an obstacle
+top_safe_area = pygame.Rect(width / 2 - 50 , 0, 100 , safe_area_height - 50)
+bottom_safe_area = pygame.Rect(0, height - safe_area_height, width, safe_area_height)
+middle_area = pygame.Rect(0, safe_area_height, width, middle_height)
+
+
+#player
+player_start_x = width // 2 - player_size // 2
+player_start_y = height - player_size
+player = pygame.Rect(player_start_x, player_start_y, player_size, player_size)
+player_bunny = pygame.Surface((player_size, player_size))
 
 #music
-background_music = pygame.mixer.Sound('music/File 4 music.mp3')
+background_music = pygame.mixer.Sound('music/music.wav')
 channel = pygame.mixer.Channel(0)
+game_over = pygame.mixer.Sound('music/game_over.wav')
+win = pygame.mixer.Sound('music/win.mp3')
 
 #images
 game_image = pygame.image.load('images/interstate.png').convert_alpha()
 game_image = pygame.transform.scale(game_image, (width, height))
 green_car = pygame.image.load('images/green_car.png').convert_alpha()
-green_car = pygame.transform.scale(green_car, (100, obstacle_height))
-orange_car = pygame.image.load('images/orange_car.png').convert_alpha()
-orange_car = pygame.transform.scale(orange_car, (100, obstacle_height))
+green_car = pygame.transform.scale(green_car, (obstacle_width - 20 , obstacle_height - 10))
+blue_car = pygame.image.load('images/blue_car.png').convert_alpha()
+blue_car = pygame.transform.scale(blue_car, (obstacle_width, obstacle_height))
+red_car = pygame.image.load('images/red_car.png').convert_alpha()
+red_car = pygame.transform.scale(red_car, (obstacle_width - 20, obstacle_height - 10))
+grey_car = pygame.image.load('images/grey_car.png').convert_alpha()
+grey_car = pygame.transform.scale(grey_car, (obstacle_width - 20, obstacle_height - 10))
+yellow_car = pygame.image.load('images/yellow_car.png').convert_alpha()
+yellow_car = pygame.transform.scale(yellow_car, (obstacle_width , obstacle_height + 10))
+siren_car = pygame.image.load('images/siren_car.png').convert_alpha()
+siren_car = pygame.transform.scale(siren_car, (obstacle_width, obstacle_height))
+cop_car = pygame.image.load('images/cop_car.png').convert_alpha()
+cop_car = pygame.transform.scale(cop_car, (obstacle_width, obstacle_height))
+jeep_car = pygame.image.load('images/jeep_car.png').convert_alpha()
+jeep_car = pygame.transform.scale(jeep_car, (obstacle_width - 5 , obstacle_height - 5))
+pink_car = pygame.image.load('images/pink_car.png').convert_alpha()
+pink_car = pygame.transform.scale(pink_car, (obstacle_width - 5, obstacle_height - 5))
+red_truck = pygame.image.load('images/red_truck.png').convert_alpha()
+red_truck = pygame.transform.scale(red_truck, (obstacle_width, obstacle_height))
+moss_car = pygame.image.load('images/moss_car.png').convert_alpha()
+moss_car = pygame.transform.scale(moss_car, (obstacle_width - 15, obstacle_height))
+burn_car = pygame.image.load('images/burn_car.png').convert_alpha()
+burn_car = pygame.transform.scale(burn_car, (obstacle_width - 10, obstacle_height))
+bunny_down = pygame.image.load('images/bunny_down.png').convert_alpha()
+bunny_down = pygame.transform.scale(bunny_down, (player_size, player_size))
+bunny_ready = pygame.image.load('images/bunny_ready.png').convert_alpha()
+bunny_ready =pygame.transform.scale(bunny_ready, (player_size, player_size))
+bunny_left = pygame.image.load('images/bunny_left.png').convert_alpha()
+bunny_left = pygame.transform.scale(bunny_left, (player_size, player_size))
+bunny_right = pygame.image.load('images/bunny_right.png').convert_alpha()
+bunny_right = pygame.transform.scale(bunny_right, (player_size, player_size))
+long_bush = pygame.image.load('images/long_bush.png').convert_alpha()
+long_bush = pygame.transform.scale(long_bush, (100, 60))
+side_bush = pygame.image.load('images/side_bush.png').convert_alpha()
+side_bush = pygame.transform.scale(side_bush, (70, 200))
+grass = pygame.image.load('images/grass.png').convert_alpha()
+grass = pygame.transform.scale(grass, (width, 200))
+plants = pygame.image.load('images/plants.png').convert_alpha()
+plants = pygame.transform.scale(plants, (50, 30))
+tall_tree = pygame.image.load('images/tall_tree.png').convert_alpha()
+tall_tree = pygame.transform.scale(tall_tree, (80, 100))
 
-vehicles = [green_car, orange_car]
+
+
+
+
+vehicles = [green_car, blue_car, red_car, grey_car, yellow_car, siren_car, cop_car, pink_car, red_truck, jeep_car]
 vehicles_swap = [
     pygame.transform.flip(green_car, True, False),
-    pygame.transform.flip(orange_car, True, False)
+    pygame.transform.flip(blue_car, True, False),
+    pygame.transform.flip(yellow_car, True, False),
+    pygame.transform.flip(siren_car, True, False),
+    pygame.transform.flip(red_car, True, False),
+    pygame.transform.flip(grey_car, True, False),
+    pygame.transform.flip(cop_car, True, False)
 ]
-
-
-top_safe_area = pygame.Rect(0, 0, width, safe_area_height)
-bottom_safe_area = pygame.Rect(0, height - safe_area_height, width, safe_area_height)
-middle_area = pygame.Rect(0, safe_area_height, width, middle_height)
 
 obstacle_images_left = []
 obstacle_images_right = []
 obstacles_right = []
 obstacles_left = []
+
+clock = pygame.time.Clock()
+channel.play(background_music)
+
+
+#methods
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
+
 # randomly selects a lane and calculates the position for a new obstacle
 def add_obstacle():
     lane = random.randint(0, lanes - 1)
-    y_position_one = (safe_area_height + lane * lane_height + lane_height) + 10
+    y_position_one = 40 + (safe_area_height + lane * lane_height)
     new_obstacle = pygame.Rect(width, y_position_one, obstacle_width, obstacle_height)
 
-    y_position_two = ((middle_height / 2 + 80) + lane * lane_height + lane_height) + 10
+    y_position_two = ((middle_height / 2 + 135) + lane * lane_height) + 5
     new_obstacle_two = pygame.Rect(0 - obstacle_width, y_position_two, obstacle_width, obstacle_height)
 
     # keeps the obstacles from overlapping
@@ -71,17 +145,11 @@ def add_obstacle():
         obstacles_right.append(new_obstacle_two)
         obstacle_images_right.append(random.choice(vehicles_swap))
 
-
-player_start_x = width // 2 - player_size // 2
-player_start_y = height - safe_area_height + (safe_area_height - player_size) // 2
-player = pygame.Rect(player_start_x, player_start_y, player_size, player_size)
-
-
 running = True
-clock = pygame.time.Clock()
-channel.play(background_music)
+player_bunny = bunny_ready
 
 while running:
+    screen.blit(game_image, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -89,19 +157,35 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT and player.left > 0:
                 player.x -= player_size
+                player_bunny = bunny_left
             elif event.key == pygame.K_RIGHT and player.right < width:
                 player.x += player_size
+                player_bunny = bunny_right
             elif event.key == pygame.K_UP and player.top > 0:
                 player.y -= player_size
+                player_bunny = bunny_ready
             elif event.key == pygame.K_DOWN and player.bottom < height:
                 player.y += player_size
+                player_bunny = bunny_down
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
 
+    #Lose Function
+    timer -= 1 / 60
+    if timer <= 0:
+        screen.fill(black)
+        game_over.play()
+        text = font50.render("GAME OVER", True, red)
+        screen.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
+        pygame.display.flip()
+        pygame.time.wait(3000)
+        running = False
+
+    #Win function
     if player.colliderect(top_safe_area):
-        screen.fill(white)  # "Enter game_over surface"
-        # "Play sound"  channel.play(dead sound) - referencing variable(dead_sound)
-        text = font.render("You Win!", True, (black))
+        screen.fill(blue)
+        channel.play(win)
+        text = font50.render("You Win!", True, (white))
         screen.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
         pygame.display.flip()
         pygame.time.wait(3000)
@@ -125,18 +209,52 @@ while running:
 
     if any(player.colliderect(obstacle) for obstacle in obstacles_left) or any(player.colliderect(obstacle) for obstacle in obstacles_right):
         screen.fill(black)
-        # "Play sound"  channel.play(dead sound) - referencing variable(dead_sound)
-        text = font.render("Game Over", True, (white))
+        channel.play(game_over)
+        text = font50.render("Game Over", True, (red))
         screen.blit(text, (width / 2 - text.get_width() / 2, height / 2 - text.get_height() / 2))
         pygame.display.flip()
         pygame.time.wait(3000)
         running = False
 
-    screen.blit(game_image, (0, 0))
-    pygame.draw.rect(screen, red, player)
-    #screen.blit(image, player)
+    # timer  and countdown settings
+    if countdown == 0:
+        time_now = pygame.time.get_ticks()
 
-#when obstacles are created for screen
+
+    if countdown > 0:
+        draw_text("GET READY!", font40, white, int(width / 2 - 110), int(height / 2 + 50))
+        draw_text(str(countdown), font40, white, int(width / 2 - 10), int(height / 2 + 100))
+        count_timer = pygame.time.get_ticks()
+        if count_timer - last_count > 1000:
+            countdown -= 1
+            last_count = count_timer
+
+    #environment images
+    screen.blit(grass, (0,  height - 125)) #bottom grass
+    screen.blit(pygame.transform.flip(grass, True, True), (0, -60)) #top grass
+    screen.blit(side_bush, (width / 2 + 25 , height - side_bush.get_height() / 2))
+    screen.blit(side_bush, (width / 2 - 100 , height - side_bush.get_height() / 2)) #right horiz bush
+    screen.blit(pygame.transform.rotate(moss_car, 20), (70, height - 150))#bottom moss car
+    screen.blit(pygame.transform.rotate(pygame.transform.flip(moss_car, True, False), -30),(width - 150 , 30 ))
+    screen.blit(pygame.transform.rotate(burn_car, -20), (width - 260 , 30 ))
+    screen.blit(long_bush, (width / 2 + 50 , height - long_bush.get_height() / 2))
+    screen.blit(long_bush, (width / 2 - 150 , height - long_bush.get_height() / 2))
+    pygame.draw.ellipse(screen, black , (width / 2 - 50, 50 , 100 , 30))
+    screen.blit(plants, (width / 2 - 50, 30))
+    screen.blit(plants, (width / 2 - 5, 30))
+    screen.blit(tall_tree, (width / 2 - 100, -10))
+    screen.blit(tall_tree, (width / 2 + 20, -10))
+    screen.blit(plants, (width / 2 + 15, 65))
+    screen.blit(plants, (width / 2 - 70, 65))
+    screen.blit(player_bunny, player)
+
+    text = font30.render(f"Time: {int(timer)}", True, (0, 0, 0))
+    screen.blit(text, (10, 10))
+    clock.tick(60)
+    previous_time = clock.get_time()
+    fps = clock.get_fps()
+
+    #obstacles are created for screen
     for obstacle, image in zip(obstacles_left, obstacle_images_left):
         screen.blit(image, obstacle)
     for obstacle, image in zip(obstacles_right, obstacle_images_right):
